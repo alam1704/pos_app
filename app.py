e92f10dc1b2bff5b7d752826ac6015afd263902a
alexl <alexlam1994@live.com>
2022-03-02 21:58:45 +1100

    Added gitignore file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from Flask import Flask, send_from_directory
+from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
