457a583e9ab79addb99a59870d22413a16550bf4
alexl <alexlam1994@live.com>
2022-03-08 17:29:15 +1100

    reorganised menu_controller

--- pos_app/controllers/menu_controller.py
+++ pos_app/controllers/menu_controller.py
@@ -7,17 +7,6 @@ from flask_login import login_user, logout_user, login_required, current_user
 # create menu_items controller
 menu_items = Blueprint('menu_items', __name__)
 
-@menu_items.route('/menu_items/', methods=['GET'])
-@login_required
-def menu_retrieve():
-    #Will retrieve an entire menu/the home page
-    menu_items=Menu.query.all()
-    data = {
-        "page_title": "Menu",
-        "menu_items": items_schema.dump(menu_items)
-    }
-    return render_template("item_menu.html", page_data=data)
-
 @menu_items.route('/')
 @login_required
 def calculator_index():
@@ -39,6 +28,17 @@ def add_item_to_cart():
     # will prompt user to enter qty then add qty and item to cart
     return "Will return item into cart"
 
+@menu_items.route('/menu_items/', methods=['GET'])
+@login_required
+def menu_retrieve():
+    #Will retrieve an entire menu/the home page
+    menu_items=Menu.query.all()
+    data = {
+        "page_title": "Menu",
+        "menu_items": items_schema.dump(menu_items)
+    }
+    return render_template("item_menu.html", page_data=data)
+
 @menu_items.route('/menu_items/<int:id>/', methods=['GET'])
 @login_required
 def item_retrieve(id):
@@ -49,23 +49,6 @@ def item_retrieve(id):
         "menu_items": item_schema.dump(menu_item)
     }
     return render_template("item_detail.html", page_data=data)
-
-@menu_items.route('/new_item/', methods=['GET'])
-@login_required
-def new_item_index():
-    data={
-        "page_title":"Add New Item"
-    }
-    return render_template("item_new.html", page_data=data)
-
-@menu_items.route('/new_item/', methods=['POST'])
-@login_required
-def item_create():
-    # Will create a specific new item on the menu
-    new_item=item_schema.load(request.form) #how do i add multiple fields to add to the menu item
-    db.session.add(new_item)
-    db.session.commit()
-    return jsonify(item_schema.dump(new_item))
 
 @menu_items.route('/menu_items/<int:id>/', methods=['PUT', 'PATCH'])
 @login_required
@@ -86,3 +69,20 @@ def item_delete(id):
     db.session.delete(menu_item)
     db.session.commit()
     return jsonify(item_schema.dump(menu_item))
+
+@menu_items.route('/new_item/', methods=['GET'])
+@login_required
+def new_item_index():
+    data={
+        "page_title":"Add New Item"
+    }
+    return render_template("item_new.html", page_data=data)
+
+@menu_items.route('/new_item/', methods=['POST'])
+@login_required
+def item_create():
+    # Will create a specific new item on the menu
+    new_item=item_schema.load(request.form) #how do i add multiple fields to add to the menu item
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify(item_schema.dump(new_item))
